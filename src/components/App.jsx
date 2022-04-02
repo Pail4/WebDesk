064d9acdfd614aa03df7134ccdff47252926b661
ksenia <devqwerty7896@protonmail.com>
2022-04-02 23:13:29 +0300

    create base-structure, start searchbar-component

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import {SearchBar} from "./SearchBar";
+
+function App() {
+
+  return (
+    <div className="App">
+      <SearchBar />
+    </div>
+  )
+}
+
+export default App

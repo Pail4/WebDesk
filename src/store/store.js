df6cf63e2f04a19d2180e04dffc960c8551ee2f1
Pail4 <73517684+Pail4@users.noreply.github.com>
2022-04-03 11:29:13 +0400

    defaultStore v.1

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+const defaultStore = {
+  blocks : [
+    {
+      id : 1,
+      name : 'YouTube',
+      position : [0, 0],
+      design : {
+        displayName : false,
+        // or
+        // name : {
+        //   display : false,
+        //   font : 'Roboto',
+        //   fontColor : '#000000'
+        // },
+        image : 'https://www.youtube.com/img/desktop/yt_1200.png',
+        displayImage : true,
+      }
+    }
+  ]
+}

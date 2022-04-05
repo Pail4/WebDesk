a5547db8f3a61012f3c668a6ab76b3eb0a864a25
Pail4 <73517684+Pail4@users.noreply.github.com>
2022-04-05 21:16:14 +0400

    blocks added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import {SearchBar} from "./SearchBar";
+import { useState } from 'react';
+import { SearchBar } from './SearchBar';
+import { BlockList } from './Blocks';
 
 function App() {
+    return (
+        <div className="App">
+            <div className="search">
+                <SearchBar apiUrl="https://www.google.com/search?q=" iconUrl="src/img/google-icon.svg" />
+                <SearchBar apiUrl="https://www.yandex.ru/search/?text=" iconUrl="src/img/yandex-icon.svg" />
+                <SearchBar apiUrl="https://duckduckgo.com/?t=ffab&q=" iconUrl="src/img/duckduckgo-icon.svg" />
+            </div>
+            <BlockList></BlockList>
+        </div>
+    );
+};
 
-  return (
-    <div className="App">
-      <div className="search">
-        <SearchBar apiUrl="https://www.google.com/search?q=" iconUrl="src/img/google-icon.svg" />
-        <SearchBar apiUrl="https://www.yandex.ru/search/?text=" iconUrl="src/img/yandex-icon.svg" />
-        <SearchBar apiUrl="https://duckduckgo.com/?t=ffab&q=" iconUrl="src/img/duckduckgo-icon.svg" />
-      </div>
-    </div>
-  )
-}
-
-export default App
+export default App;
--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,68 @@
-const defaultStore = {
-  blocks : [
-    {
-      id : 1,
-      name : 'YouTube',
-      position : [0, 0],
-      design : {
-        name : {
-          display : false,
-          fontColor : '#000000'
+export const defaultStore = {
+    blocks: [
+        {
+            id: 1,
+            name: 'YouTube',
+            position: ['1', '1'],
+            design: {
+                name: {
+                    displayName: false,
+                    fontColor: '#000000'
+                },
+                background: {
+                    displayImage: true,
+                    image: 'https://www.youtube.com/img/desktop/yt_1200.png',
+                    color: '#fff'
+                }
+            }
         },
-        background : {
-          display : true,
-          image : 'https://www.youtube.com/img/desktop/yt_1200.png',
-          color : '#000000'
+        {
+            id: 2,
+            name: 'YouTube',
+            position: ['2', '1 / 3'],
+            design: {
+                name: {
+                    displayName: false,
+                    fontColor: '#000000'
+                },
+                background: {
+                    displayImage: true,
+                    image: 'https://www.youtube.com/img/desktop/yt_1200.png',
+                    color: '#fff'
+                }
+            }
+        },
+        {
+            id: 3,
+            name: 'YouTube',
+            position: ['3 / 5', '1'],
+            design: {
+                name: {
+                    displayName: true,
+                    fontColor: '#000000'
+                },
+                background: {
+                    displayImage: false,
+                    image: 'https://www.youtube.com/img/desktop/yt_1200.png',
+                    color: '#fff'
+                }
+            }
+        },
+        {
+            id: 4,
+            name: 'YouTube',
+            position: ['6', '1'],
+            design: {
+                name: {
+                    displayName: false,
+                    fontColor: '#000000'
+                },
+                background: {
+                    displayImage: true,
+                    image: 'https://www.youtube.com/img/desktop/yt_1200.png',
+                    color: '#fff'
+                }
+            }
         }
-      }
-    }
-  ]
-}
+    ]
+};
